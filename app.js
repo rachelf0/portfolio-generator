233d6aa9736254bcec29cf64eadb8f4a08402b6c
Rachel Fritz <rachelfritz0@gmail.com>
2020-07-28 14:08:36 -0500

    created new src folder

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs');
 
-const printProfileData = (profileDataArr) => {
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-        console.log(profileDataArr[i]);
-    }
-    console.log('================');
+const generatePage = require('./src/page-template.js');
 
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+const profileDataArgs = process.argv.slice(2);
 
-printProfileData(profileDataArgs);
+const [name, github] = profileDataArgs;
+
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if (err) throw new Error(err);
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
+
+// console.log(profileDataArgs);
+
+// const printProfileData = (profileDataArr) => {
+//     for (let i = 0; i < profileDataArr.length; i += 1) {
+//         console.log(profileDataArr[i]);
+//     }
+//     console.log('================');
+
+//     profileDataArr.forEach(profileItem => console.log(profileItem));
+// };
+
+// printProfileData(profileDataArgs);
